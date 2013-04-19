64ccc871f8346c688cb218248bee942b5db5767a
xyubuntu@gmail.com <xyubuntu@gmail.com@93f61abd-545e-89fb-fa80-50afc56cc2ec>
2013-04-19 17:18:03 +0000

    添加时间管理类TimeUtil实现文件

---
+++ trunk/src/com/yaoli/smsread/TimeUtil.java
@@ -0,0 +1,27 @@
+package com.yaoli.smsread;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+public class TimeUtil {
+	private long longTime;
+	private String yearMonth;
+	public TimeUtil(long lTime)
+	{
+		longTime = lTime;
+	}
+	public String toLabel()
+	{
+		SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd", Locale.US);
+		Date d = new Date(longTime);
+		yearMonth = dateFormat.format(d);
+		Date today = new Date(System.currentTimeMillis());
+		if(yearMonth.equals(dateFormat.format(today)))
+		{
+			dateFormat = new SimpleDateFormat("hh:mm", Locale.US);
+			return dateFormat.format(d);
+		}
+		return yearMonth; 
+	}
+}
