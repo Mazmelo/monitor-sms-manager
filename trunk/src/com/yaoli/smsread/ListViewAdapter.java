ebf6476cbeaffca94e58d5554e4759c0f9938616
xyubuntu@gmail.com <xyubuntu@gmail.com@93f61abd-545e-89fb-fa80-50afc56cc2ec>
2013-04-26 15:53:05 +0000

    增加未读标识-未完成

--- trunk/src/com/yaoli/smsread/ListViewAdapter.java
+++ trunk/src/com/yaoli/smsread/ListViewAdapter.java
@@ -71,7 +71,7 @@ public class ListViewAdapter extends BaseAdapter{
 		holder.imageCreateMsg = (ImageView)convertView.findViewById(R.id.CreateImage);
 		holder.tv_create_msg = (TextView)convertView.findViewById(R.id.CreateMsg);
 		holder.imageCreateMsg.setBackgroundResource(R.drawable.create_icon);
-		holder.tv_create_msg.setText("��������Ϣ");
+		holder.tv_create_msg.setText("新建短消息");
 	}
 	public View setupHolder(int position, View convertView, ViewHolder holder)
 	{
